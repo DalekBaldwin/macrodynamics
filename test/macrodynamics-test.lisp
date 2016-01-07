80ef1e4a343b7a31498b913caf30da9c1c9d8510
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2016-01-06 22:29:48 -0500

    test dynenv-macrolet

--- test/macrodynamics-test.lisp
+++ test/macrodynamics-test.lisp
@@ -19,14 +19,15 @@
                            (format t "~&~A ~A ~A ~A~%" x y a b)
                            (= a a))))))))))
 
-#+nil
-(with-generators ((x (generator (integer)))
-                  (y (generator (integer))))
-  (let-map ((a (+ x x)))
-    (let ((z 3))
-      (wrap-let-map ((b (+ a a)))
-        (let ((check-it:*num-trials* 10))
-          (is
-           (check-that (progn
-                         (format t "~&~A ~A ~A ~A~%" x y a b)
-                         (= a a)))))))))
+(def-dynenv-var **test-var** 0)
+
+(deftest test-dynenv-macrolet ()
+  (is
+   (equal
+    (dynenv-macrolet ((herp (&body body)
+                            (ct-let ((**test-var** (1+ **test-var**)))
+                              `(progn ,@body)))
+                      (derp (thing)
+                            `(list* ,**test-var** ,thing)))
+      (derp (herp (derp (herp (derp (herp (derp (herp (derp nil))))))))))
+    (list 0 1 2 3 4))))
