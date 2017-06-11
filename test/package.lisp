fd7c10289813ad9efc38dd4b2d326c374dea1978
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2017-06-11 17:33:18 -0400

    make fiasco play nice with abcl

--- test/package.lisp
+++ test/package.lisp
@@ -1,11 +1,15 @@
 (in-package :cl-user)
 
-(fiasco:define-test-package :macrodynamics/test
+(defpackage :macrodynamics/test
   (:use :cl
         :macrodynamics
         :fiasco
         :alexandria
         :check-it))
+
+(fiasco:defsuite
+    (fiasco-suites::macrodynamics/test :bind-to-package :macrodynamics/test
+                                       :in fiasco-suites::all-tests))
 
 (in-package :macrodynamics/test)
 
